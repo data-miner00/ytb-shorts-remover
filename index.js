e7b61168b452a080d8fcdcca71d9a2cb77a84c84
data-miner00 <data-miner00@users.noreply.github.com>
2024-02-12 09:44:43 +0800

    feat: Remove entire sidebar and menu toggle

--- index.js
+++ index.js
@@ -4,23 +4,33 @@
   const RICH_SECTION_RENDERER = "ytd-rich-section-renderer";
   const SEARCH_SHORTS_RENDERER = "ytd-reel-shelf-renderer";
   const SHORTS_SIDEBAR_BUTTON = "#items ytd-guide-entry-renderer:nth-child(2)";
+  const WHOLE_SIDEBAR = "tp-yt-app-drawer";
+  const PAGE_MANAGER = "ytd-page-manager#page-manager";
+  const MINI_SIDEBAR = "ytd-mini-guide-renderer";
+  const SIDEBAR_BURGER_TOGGLE = "yt-icon-button#guide-button";
   const HOMEPAGE_CHECKBOX = "homepage";
   const SEARCHPAGE_CHECKBOX = "searchpage";
   const SIDEBAR_CHECKBOX = "sidebar";
+  const SIDEBAR_ALL_CHECKBOX = "sidebar-all";
 
   let results = await browser.storage.local.get(),
     homepage = results[HOMEPAGE_CHECKBOX],
     searchpage = results[SEARCHPAGE_CHECKBOX],
-    sidebar = results[SIDEBAR_CHECKBOX];
+    sidebar = results[SIDEBAR_CHECKBOX],
+    sidebarAll = results[SIDEBAR_ALL_CHECKBOX];
 
   if (homepage === undefined) homepage = true;
   if (searchpage === undefined) searchpage = true;
   if (sidebar === undefined) sidebar = true;
+  if (sidebarAll === undefined) sidebarAll = true;
 
   const removeList = [
     homepage && RICH_SECTION_RENDERER,
     searchpage && SEARCH_SHORTS_RENDERER,
     sidebar && SHORTS_SIDEBAR_BUTTON,
+    sidebarAll && WHOLE_SIDEBAR,
+    sidebarAll && MINI_SIDEBAR,
+    sidebarAll && SIDEBAR_BURGER_TOGGLE,
   ].filter((x) => x);
 
   var mutationObserver = new MutationObserver(() => {
@@ -29,4 +39,14 @@
   });
 
   mutationObserver.observe(document.body, { subtree: true, childList: true });
+
+  if (sidebarAll) {
+    var YTD_APP = "ytd-app";
+    var PERSISTENT_GUIDE_PADDING_STYLE = "--ytd-persistent-guide-width";
+    var width = "0";
+
+    var ytdAppContainer = document.querySelector(YTD_APP);
+
+    ytdAppContainer?.style.setProperty(PERSISTENT_GUIDE_PADDING_STYLE, width);
+  }
 })();
--- popup.js
+++ popup.js
@@ -3,21 +3,24 @@
 const HOMEPAGE_CHECKBOX = "homepage";
 const SEARCHPAGE_CHECKBOX = "searchpage";
 const SIDEBAR_CHECKBOX = "sidebar";
+const SIDEBAR_ALL_CHECKBOX = "sidebar-all";
 
 const homepageCheckbox = document.querySelector('[name="homepage"]');
 const searchpageCheckbox = document.querySelector('[name="searchpage"]');
 const sidebarCheckbox = document.querySelector('[name="sidebar"]');
+const sidebarAllCheckbox = document.querySelector('[name="sidebar-all"]');
 
-homepageCheckbox.addEventListener("change", (event) => {
-  setValue(HOMEPAGE_CHECKBOX, event.target.checked);
-});
+const checkboxes = [
+  [homepageCheckbox, HOMEPAGE_CHECKBOX],
+  [searchpageCheckbox, SEARCHPAGE_CHECKBOX],
+  [sidebarCheckbox, SIDEBAR_CHECKBOX],
+  [sidebarAllCheckbox, SIDEBAR_ALL_CHECKBOX],
+];
 
-searchpageCheckbox.addEventListener("change", (event) => {
-  setValue(SEARCHPAGE_CHECKBOX, event.target.checked);
-});
-
-sidebarCheckbox.addEventListener("change", (event) => {
-  setValue(SIDEBAR_CHECKBOX, event.target.checked);
+checkboxes.forEach((entries) => {
+  entries[0].addEventListener("change", (event) => {
+    setValue(entries[1], event.target.checked);
+  });
 });
 
 const button = document.querySelector("button");
@@ -32,19 +35,23 @@ async function init() {
   let results = await browser.storage.local.get(),
     homepage = results[HOMEPAGE_CHECKBOX],
     searchpage = results[SEARCHPAGE_CHECKBOX],
-    sidebar = results[SIDEBAR_CHECKBOX];
+    sidebar = results[SIDEBAR_CHECKBOX],
+    sidebarAll = results[SIDEBAR_ALL_CHECKBOX];
 
   if (homepage === undefined) homepage = true;
   if (searchpage === undefined) searchpage = true;
   if (sidebar === undefined) sidebar = true;
+  if (sidebarAll === undefined) sidebarAll = true;
 
   homepageCheckbox.checked = homepage;
   searchpageCheckbox.checked = searchpage;
   sidebarCheckbox.checked = sidebar;
+  sidebarAllCheckbox.checked = sidebarAll;
 
   setValue(HOMEPAGE_CHECKBOX, homepage);
   setValue(SEARCHPAGE_CHECKBOX, searchpage);
   setValue(SIDEBAR_CHECKBOX, sidebar);
+  setValue(SIDEBAR_ALL_CHECKBOX, sidebarAll);
 }
 
 function openInNewTab() {
