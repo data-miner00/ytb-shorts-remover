3bed9d54e9638d05da4d2202eca5fcb6ce386dcd
data-miner00 <data-miner00@users.noreply.github.com>
2024-02-12 23:11:07 +0800

    feat: Add to top button

--- index.js
+++ index.js
@@ -50,4 +50,32 @@
 
     ytdAppContainer?.style.setProperty(PERSISTENT_GUIDE_PADDING_STYLE, width);
   }
+
+  createToTopButton();
 })();
+
+function createToTopButton() {
+  var toTopButton = document.createElement("a");
+  var s = toTopButton.style;
+
+  toTopButton.href = "#";
+  toTopButton.innerHTML = `<svg style="display: block;" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5"/>
+</svg>`;
+  s.position = "fixed";
+  s.bottom = "10px";
+  s.right = "10px";
+  s.height = "30px";
+  s.width = "30px";
+  s.background = "red";
+  s.color = "white";
+  s.padding = "4px";
+  s.fontSize = "16px";
+  s.borderRadius = "999px";
+  s.display = "flex";
+  s.justifyContent = "center";
+  s.alignItems = "center";
+  s.zIndex = "10";
+
+  document.body.appendChild(toTopButton);
+}
