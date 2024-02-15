68a242eb780f893b5da38e7c5eb1afdde674b70c
data-miner00 <data-miner00@users.noreply.github.com>
2024-02-15 10:34:49 +0800

    feat: Remove playlist header

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
   const PAGE_MANAGER = "ytd-page-manager#page-manager";
   const MINI_SIDEBAR = "ytd-mini-guide-renderer";
   const SIDEBAR_BURGER_TOGGLE = "yt-icon-button#guide-button";
+  const PLAYLIST_HEADER = "ytd-playlist-header-renderer";
 
   const HOMEPAGE_CHECKBOX = "homepage";
   const SEARCHPAGE_CHECKBOX = "searchpage";
@@ -32,6 +33,7 @@
     sidebarAll && WHOLE_SIDEBAR,
     sidebarAll && MINI_SIDEBAR,
     sidebarAll && SIDEBAR_BURGER_TOGGLE,
+    sidebarAll && PLAYLIST_HEADER,
   ].filter((x) => x);
 
   var mutationObserver = new MutationObserver(() => {
@@ -62,6 +64,7 @@ function createToTopButton() {
   toTopButton.innerHTML = `<svg style="display: block;" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5"/>
 </svg>`;
+  toTopButton.title = "Scroll to top";
   s.position = "fixed";
   s.bottom = "10px";
   s.right = "10px";
